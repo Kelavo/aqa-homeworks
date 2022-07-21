ea53615838ffd084218fc29556ac0755f09a944f
kelavo <diak0n@mail.ru>
2022-07-21 23:27:24 +0300

    Add Junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+    public void shouldShowAmountToCashback() {
+        int actual = cashback.remain(900);
+        int expected = 100;
+
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldShowAmountToCashbackBorder() {
+        int actual = cashback.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual,expected);
+    }
+}
